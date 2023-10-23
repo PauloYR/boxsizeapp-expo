ba77484800c322d213eb10186db290f3e671b661
Paulo Rocha <pauloyr@outlook.com>
2023-10-23 20:07:30 -0300

    add style text

---
+++ src/styles/text.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const stylesText = StyleSheet.create({
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: '#000'
+    }
+})
+
+export default stylesText
