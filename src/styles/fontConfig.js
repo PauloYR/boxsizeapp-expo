57fcf51f7cdb6335e0b6ea3e3be3a5a00855187f
João Rafael <jb.rafaelt@gmail.com>
2023-10-30 19:51:13 -0300

    Inclusão para ajuste de fontes

--- src/styles/fontConfig.js
+++ src/styles/fontConfig.js
@@ -3,6 +3,8 @@ import * as Font from 'expo-font';
 export const loadFonts = async () => {
   await Font.loadAsync({
     'RobotoSerif-SemiBold': require('../assets/fonts/static/RobotoSerif-SemiBold.ttf'),
+    'RobotoSerif-Bold': require('../assets/fonts/static/RobotoSerif-Bold.ttf'),
+    'RobotoSerif-SemiBoldItalic': require('../assets/fonts/static/RobotoSerif-SemiBoldItalic.ttf'),
     'RobotoSerif-Regular': require('../assets/fonts/static/RobotoSerif-Regular.ttf'),
   });
 };
